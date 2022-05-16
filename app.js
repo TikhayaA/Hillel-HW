e5eae3ddbdbff304f1a6348c592628c34cd5e4e7
ATikha <tikhaya.a.v@gmail.com>
2022-05-17 00:45:18 +0300

    HW9

--- app.js
+++ app.js
@@ -1,17 +1,59 @@
-const btnE = document.querySelector('.btn');
-const inpE = document.querySelector('.inp');
-const listE = document.querySelector('.list');
+const adminName = document.getElementById("admin-name");
+const adminLastName = document.getElementById("admin-last-name");
+const adminNumber = document.getElementById("admin-number");
+const tableList = document.querySelector(".table__list");
+const btn = document.querySelector(".admin__btn");
 
 
+btn.addEventListener("click", addTable);
 
-btnE.addEventListener("click", onClick);
+function addTable(){
 
-function onClick(){
-    let liE = document.createElement('li');
-    liE.innerHTML = inpE.value;
-    listE.appendChild(liE);
-    inpE.value = "";
-    console.log(liE);
+    const listName = adminName.value;
+    const listlastName = adminLastName.value;
+    const listNumber = adminNumber.value;
 
+    if (!listName.trim() || !listlastName.trim() || !listNumber.trim()){
+        alert('Please fill empty field!');
+        return;
+    }
+
+
+    const newContact = document.createElement('li');
+    const newItemName = document.createElement('div');
+    const newItemLastName = document.createElement('div');
+    const newItemNumber = document.createElement('div');
+
+
+    if ( /[A-Za-z]/.test(adminName.value) && /[A-Za-z]/.test(adminLastName.value) && /[0-9]/.test(adminNumber.value)) {
+
+        addHtml(newItemName, adminName);
+        addHtml(newItemLastName, adminLastName);
+        addHtml(newItemNumber, adminNumber);
+
+        createChild(newContact, newItemName);
+        createChild(newContact, newItemLastName);
+        createChild(newContact, newItemNumber);
+        createChild(tableList, newContact);
+
+        newContact.classList.add('table__item');
+
+        clearValue(adminName);
+        clearValue(adminLastName);
+        clearValue(adminNumber);
+        
+    } else {
+       alert ('input error');
+   }
+}
+function createChild(p, ch){
+    p.appendChild(ch);
 }
 
+function addHtml(el, val){
+    el.innerHTML = val.value;
+}
+
+function clearValue(inpValue) {
+    inpValue.value = "";
+}
