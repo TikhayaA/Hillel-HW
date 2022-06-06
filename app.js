8f60615113b2458b5a26a887dd841c7331ee0dad
ATikha <tikhaya.a.v@gmail.com>
2022-06-06 19:10:41 +0300

    lesson 10 createContactElement

--- app.js
+++ app.js
@@ -1,57 +1,42 @@
-const adminName = document.getElementById("admin-name");
-const adminLastName = document.getElementById("admin-last-name");
-const adminNumber = document.getElementById("admin-number");
-const tableList = document.querySelector(".table__list");
-const btn = document.querySelector(".admin__btn");
+const inputsE = document.querySelectorAll('inputs-user');
+
+const name = document.getElementById("name");
+const lastName = document.getElementById("last-Name");
+const phone = document.getElementById("phone");
+
+const formList = document.querySelector(".form--list-wrap");
+const btn = document.querySelector(".form--btn");
 
 
 btn.addEventListener("click", addTable);
 
-function addTable(){
+function addTable(){ 
 
-    const listName = adminName.value;
-    const listlastName = adminLastName.value;
-    const listNumber = adminNumber.value;
+    const listName = name.value;
+    const listlastName = lastName.value;
+    const listNumber = phone.value;
 
     if (!listName.trim() || !listlastName.trim() || !listNumber.trim()){
         alert('Please fill empty field!');
+        clearValue(inputsE);
         return;
     }
+    const el = createContactElement(listName, listlastName, listNumber, 'li', "item-list");
+    addHtml(formList, el);
+    clearValue(inputsE);
 
-
-    const newContact = document.createElement('li');
-    const newItemName = document.createElement('div');
-    const newItemLastName = document.createElement('div');
-    const newItemNumber = document.createElement('div');
-
-
-    if ( /[A-Za-z]/.test(adminName.value) && /[A-Za-z]/.test(adminLastName.value) && /[0-9]/.test(adminNumber.value)) {
-
-        addHtml(newItemName, adminName);
-        addHtml(newItemLastName, adminLastName);
-        addHtml(newItemNumber, adminNumber);
-
-        createChild(newContact, newItemName);
-        createChild(newContact, newItemLastName);
-        createChild(newContact, newItemNumber);
-        createChild(tableList, newContact);
-
-        newContact.classList.add('table__item');
-
-        clearValue(adminName);
-        clearValue(adminLastName);
-        clearValue(adminNumber);
-        
-    } else {
-       alert ('input error');
-   }
 }
-function createChild(p, ch){
-    p.appendChild(ch);
+function createContactElement(name, lastName, phone,containerTag, containerClassname){
+    return `<${containerTag} class="${containerClassname}">
+                <div>${name}</div>
+                <div>${lastName}</div>
+                <div>${phone}</div>
+            </${containerTag}>`
 }
 
 function addHtml(el, val){
-    el.innerHTML = val.value;
+    console.log(el,val)
+    el.innerHTML += val;
 }
 
 function clearValue(inpValue) {
